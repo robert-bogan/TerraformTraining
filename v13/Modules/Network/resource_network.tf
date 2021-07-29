61300e9cbdde35d1acee9e7145324b60db3f5b89
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-07-29 20:08:42 +0100

    Update

---
+++ v13/Modules/Network/resource_network.tf
@@ -0,0 +1,50 @@
+# Create virtual network
+resource "azurerm_virtual_network" "vm_network" {
+  name                = var.network_name
+  address_space       = [var.network_address]
+  resource_group_name = azurerm_resource_group.vm_group.name
+  location            = azurerm_resource_group.vm_group.location
+}
+
+# Create network subnet
+resource "azurerm_subnet" "vm_subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.vm_group.name
+  virtual_network_name = azurerm_virtual_network.vm_network.name
+  address_prefixes     = cidrsubnets(var.network_address, 1)
+}
+
+resource "azurerm_route_table" "route_table" {
+  name                          = var.subnet_name
+  resource_group_name           = azurerm_resource_group.vm_group.name
+  location                      = azurerm_resource_group.vm_group.location
+  disable_bgp_route_propagation = false
+}
+
+resource "azurerm_route" "udr_internet" {
+  resource_group_name           = azurerm_resource_group.vm_group.name
+  route_table_name    = azurerm_route_table.route_table.name
+  name                = "UDR-Internet"
+  address_prefix      = "0.0.0.0/0"
+  next_hop_type       = "Internet"
+}
+
+resource "azurerm_subnet_route_table_association" "subnet_association" {
+  subnet_id      = azurerm_subnet.vm_subnet.id
+  route_table_id = azurerm_route_table.route_table.id
+}
+
+resource "azurerm_network_security_group" "network_security_group" {
+  name                = var.subnet_name
+  resource_group_name           = azurerm_resource_group.vm_group.name
+  location                      = azurerm_resource_group.vm_group.location
+}
+
+resource "azurerm_subnet_network_security_group_association" "network_security_group_association" {
+  # Explicit dependency to prevent race condition of the route table being associated in parallel
+  depends_on = [
+    azurerm_subnet_route_table_association.subnet_association
+  ]
+  subnet_id      = azurerm_subnet.vm_subnet.id
+  network_security_group_id = azurerm_network_security_group.network_security_group.id
+}
---
+++ v13/variables.tf
@@ -0,0 +1,42 @@
+variable "resource_group" {
+  description = "Name of resource group"
+  type        = string
+  default     = "VirtualMachineGroup"
+}
+
+variable "resource_location" {
+  description = "Location of resources"
+  type        = list(string)
+  default = [
+    "UK South",
+    "North Central US"
+  ]
+}
+
+variable "vm_name" {
+  description = "Name of vm"
+  type        = string
+  default     = "VM"
+}
+
+variable "vm_size" {
+  description = "Size of vm"
+  type        = string
+  default     = "Standard_B1s"
+}
+
+variable "vm_instance_count" {
+  description = "Number of vms to deploy"
+  type        = string
+  default     = 2
+}
+
+variable "network_address" {
+  description = "Network address space"
+  type        = map(string)
+  default = {
+    "UK South"         = "10.0.0.0/22"
+    "North Europe"     = "10.1.0.0/22"
+    "North Central US" = "10.2.0.0/22"
+  }
+}
