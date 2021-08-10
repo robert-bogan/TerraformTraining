170f1c597672dd951f3a15b6032190ffdcbdceab
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2021-08-10 11:55:36 +0100

    Update

--- v23/resources.tf
+++ v23/resources.tf
@@ -17,8 +17,8 @@ module "vm_deployment_traffic_manager" {
   vm_name                            = var.vm_name
   traffic_manager_name               = "${var.vm_name}-TM"
   traffic_manager_location           = var.traffic_manager_location
-  traffic_manager_endpoint_locations = var.resource_location
   traffic_manager_endpoints          = module.vm_deployment
+  traffic_manager_endpoint_locations = var.resource_location
 }
 
 # Create production recovery services vault
